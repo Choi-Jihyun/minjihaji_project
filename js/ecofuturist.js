b0a01e7649e91d9d1d0bc4c59cded1f4ca1c70c9
osung_jihyun <osungcjh@gmail.com>
2023-06-28 11:26:25 +0900

    work1_markup_complete

---
+++ js/ecofuturist.js
@@ -0,0 +1,119 @@
+// // 지현
+document.addEventListener('DOMContentLoaded', ()=>{
+  const logoInteractionWrap = document.querySelector("#logo_interaction_wrap");
+  const brick = document.querySelector("#brick");
+  const logoGlobe =document.querySelector("#logo_globe")
+  const logoGlobeSpin = document.querySelector("#logo_globe_spin");
+  const logoTexts = document.querySelector(".logo_texts");
+  const holeBack = document.querySelector("#hole_back");
+  const holeFront = document.querySelector("#hole_front");
+  const waveWrap = document.querySelector("#wave_wrap");
+  const floatTexts = document.querySelectorAll(".float_text");
+  const logoSpinWrap = document.querySelector("#logo_spin_wrap");
+  
+  floatAnimate();
+
+  function floatAnimate() {
+    floatTexts.forEach((text, index) => {
+      gsap.to(text, {
+        y: `+=20`,
+        duration: 2,
+        ease: CustomEase.create("custom", "M0,0 C0.176,-0.152 0.374,0.306 0.507,0.512 0.652,0.738 0.852,1.152 1,1 "),
+        repeat: -1,
+        yoyo: true,
+        delay: index * 0.3 // 0.5초마다 요소들이 움직이도록 함
+      })
+    });
+  }
+
+
+
+  
+  
+
+  gsap.set(waveWrap, {display: "none"})
+
+  gsap.to(brick, {
+    top: 93.4+'vh',
+    scrollTrigger: {
+      trigger: logoInteractionWrap,
+      markers: true,
+      start: 'top -10%',
+      end: 'bottom 120%', 
+      scrub: 1,
+    },
+    onComplete: ()=>{
+      gsap.to(holeBack, {display: 'none', duration: 0.1, onComplete: ()=> { 
+        // 추가
+        gsap.to(brick, {opacity: 0, duration: 0.1, onComplete: ()=>{
+          gsap.to(holeFront, {display: 'none'})
+        }})
+      }}) 
+      gsap.to(waveWrap, {display: "block", bottom: 46+'vh', duration: 0.7})
+      
+    }
+  })
+
+  gsap.to(logoGlobe, {
+    top: 120+'vh',
+    scrollTrigger: {
+      trigger: logoInteractionWrap,
+      // markers: true,
+      start: 'top -10%',
+      end: 'bottom 120%',
+      scrub: 1,
+    },
+    onComplete: ()=>{
+      gsap.to(logoGlobe, {display: 'none', duration: 0.2})
+    }
+  })
+
+
+  let rotationGlobe = 100;
+  let speedGlobe = 20;
+
+  setInterval(animate, 100);
+
+  function animate() {
+    rotationGlobe += speedGlobe;
+    logoGlobeSpin.style.transform = `rotateY(${rotationGlobe}deg)`;
+  }
+  
+  gsap.to(logoGlobeSpin, {
+    scrollTrigger: {
+      trigger: logoInteractionWrap,
+      // markers: true,
+      start: 'top -10%',
+      end: 'bottom 120%',
+      scrub: 1,
+    }
+  })
+
+  gsap.to(logoTexts, {
+    top: 140+'vh',
+    scrollTrigger: {
+      trigger: logoInteractionWrap,
+      // markers: true,
+      start: 'top -10%',
+      end: 'bottom 120%',
+      scrub: 1,
+    },
+    onComplete: ()=>{
+      gsap.to(logoTexts, {display: 'none', duration: 0.2})
+    }
+  })
+
+  // logoSpinWrap
+  let rotation = 100;
+  let speed = 20;
+
+  setInterval(smallLogoAnimate, 100); // 10밀리 초마다 animate 함수 실행
+
+  function smallLogoAnimate() {
+    rotation += speed;
+    logoSpinWrap.style.transform = `rotateY(${rotation}deg)`;
+  }
+
+
+
+})
---
+++ js/main.js
@@ -0,0 +1,122 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // 지현
+  const svgPath = document.querySelector("#logo_svg_wrap > svg > path");
+  svgPath.classList.add('animation')
+
+
+  //[지혜수정_0627]
+  const $logoBox = document.querySelector('#logo_box')
+  const $mainTitle = document.querySelector('#title')
+  const $changeTitle = document.querySelector('#changeTitle')
+  const $mainLogo = document.querySelector('#main_logo')
+
+  let logoDuration = 1;
+
+  
+
+  $logoBox.addEventListener('click', activateTitle)
+
+  // activateTitle();
+
+  function activateTitle() {//0167
+
+    gsap.set($changeTitle, { display: 'block', top: 60 + 'px', left: -90 + 'px' })
+    gsap.to($changeTitle.children[10], { left: 1509.88 + 'px', ease: 'power1.out', duration: logoDuration, delay: 0.2 })
+
+    gsap.to($mainTitle.children[11], { top: 76 + 'px', duration: logoDuration, delay: 1 })
+    gsap.to($mainTitle.children[11], { left: 801.64 + 'px', duration: logoDuration, delay: 1.5 })
+    gsap.to($mainTitle.children[11], { left: 690 + 'px', duration: logoDuration, delay: 2 })
+    gsap.to($mainTitle.children[11], { left: 350 + 'px', duration: logoDuration, delay: 2.5 })
+    gsap.to($mainTitle.children[11], { left: 315.43 + 'px', duration: logoDuration, delay: 3 })
+    gsap.to($mainTitle.children[11], { top: 0 + 'px', left: 122.5 + 'px', duration: logoDuration, delay: 4 })
+
+    gsap.to($mainTitle.children[10], { top: 76 + 'px', duration: logoDuration, delay: 1 })
+    gsap.to($mainTitle.children[10], { left: 718.15 + 'px', duration: logoDuration, delay: 1.5 })
+    gsap.to($mainTitle.children[10], { left: 620 + 'px', duration: logoDuration, delay: 2 })
+    gsap.to($mainTitle.children[10], { left: 290 + 'px', duration: logoDuration, delay: 2.5 })
+    gsap.to($mainTitle.children[10], { left: 280 + 'px', duration: logoDuration, delay: 3 })
+    gsap.to($mainTitle.children[10], { left: 280 + 'px', duration: logoDuration, delay: 3 })
+    gsap.to($mainTitle.children[10], { top: 0 + 'px', left: 0 + 'px', duration: logoDuration, delay: 4 })
+    gsap.to($mainTitle.children[10], { left: 20 + 'px', duration: logoDuration, delay: 6.5 })
+
+
+    gsap.to($mainTitle.children[0], { top: -76 + 'px', duration: logoDuration, delay: 1.5 })
+    gsap.to($mainTitle.children[0], { left: 233.81 + 'px', duration: logoDuration, delay: 2 })
+    gsap.to($mainTitle.children[0], { left: 374.04 + 'px', duration: logoDuration, delay: 2.5 })
+    gsap.to($mainTitle.children[0], { left: 477.46 + 'px', duration: logoDuration, delay: 3 })
+    gsap.to($mainTitle.children[0], { left: 500 + 'px', duration: logoDuration, delay: 4.5 })
+    gsap.to($mainTitle.children[0], { left: 800 + 'px', duration: logoDuration, delay: 5 })
+    gsap.to($mainTitle.children[0], { left: 1500 + 'px', duration: logoDuration, delay: 5.5 })
+    gsap.to($mainTitle.children[0], { left: 2000 + 'px', duration: logoDuration, delay: 5.8 })
+
+    gsap.to($mainTitle.children[1], { top: -76 + 'px', duration: logoDuration, delay: 1.5 })
+    gsap.to($mainTitle.children[1], { left: 315.43 + 'px', duration: logoDuration, delay: 2 })
+    gsap.to($mainTitle.children[1], { left: 477.46 + 'px', duration: logoDuration, delay: 2.5 })
+    gsap.to($mainTitle.children[1], { left: 560.95 + 'px', duration: logoDuration, delay: 3 })
+    gsap.to($mainTitle.children[1], { top: 0 + 'px', left: 642.95 + 'px', duration: logoDuration, delay: 4 })
+
+    gsap.to($mainTitle.children[6], { top: -76 + 'px', duration: logoDuration, delay: 1.5 })
+
+    gsap.to($mainTitle.children[7], { top: -76 + 'px', duration: logoDuration, delay: 1.5 })
+    gsap.to($mainTitle.children[7], { left: 740 + 'px', duration: logoDuration, delay: 2 })
+    gsap.to($mainTitle.children[7], { left: 984.88 + 'px', duration: logoDuration, delay: 2.5 })
+    gsap.to($mainTitle.children[7], { left: 1100 + 'px', duration: logoDuration, delay: 3 })
+    gsap.to($mainTitle.children[7], { left: 1300 + 'px', duration: logoDuration, delay: 3.5 })
+    gsap.to($mainTitle.children[7], { left: 1500+ 'px', duration: logoDuration, delay: 4 })
+    gsap.to($mainTitle.children[7], { left: 1800 + 'px', duration: logoDuration, delay: 4.5 })
+    gsap.to($mainTitle.children[7], { left: 2000 + 'px', duration: logoDuration, delay: 5 })
+    // gsap.to($mainTitle.children[7], { left: 900 + 'px', duration: logoDuration, delay: 5.5 })
+    // gsap.to($mainTitle.children[7], { left: 2000 + 'px', duration: logoDuration, delay: 5.8 })
+
+    gsap.to($mainTitle.children[8], { top: -76 + 'px', duration: logoDuration, delay: 2.2 })
+    gsap.to($mainTitle.children[8], { left: 893.26 + 'px', duration: logoDuration, delay: 2.5 })
+    gsap.to($mainTitle.children[8], { top: 0 + 'px', duration: logoDuration, delay: 3 })
+    gsap.to($mainTitle.children[8], { left: 801.64 + 'px', duration: logoDuration, delay: 6.5 })
+
+    gsap.to($mainTitle.children[9], { top: 76 + 'px', duration: logoDuration, delay: 2.5 })
+    gsap.to($mainTitle.children[9], { left: 642.95 + 'px', duration: logoDuration, delay: 3.3 })
+    gsap.to($mainTitle.children[9], { top: 0 + 'px', left: 560.95 + 'px', duration: logoDuration, delay: 4 })
+
+    gsap.to($mainTitle.children[2], { top: -40 + 'px', duration: logoDuration, delay: 3.5 })
+    gsap.to($mainTitle.children[2], { top: -76 + 'px', duration: logoDuration, delay: 3.8 })
+    gsap.to($mainTitle.children[2], { left: 233.81 + 'px', duration: logoDuration, delay: 4 })
+    gsap.to($mainTitle.children[2], { left: 374.04 + 'px', duration: logoDuration, delay: 4.5 })
+    gsap.to($mainTitle.children[2], { left: 950 + 'px', duration: logoDuration, delay: 5 })
+    gsap.to($mainTitle.children[2], { left: 1300 + 'px', duration: logoDuration, delay: 5.5 })
+    gsap.to($mainTitle.children[2], { left: 2000 + 'px', duration: logoDuration, delay: 5.8 })
+
+    gsap.to($mainTitle.children[3], { top: -40 + 'px', duration: logoDuration, delay: 3.5 })
+    gsap.to($mainTitle.children[3], { top: -76 + 'px', duration: logoDuration, delay: 3.8 })
+    gsap.to($mainTitle.children[3], { left: 315.43 + 'px', duration: logoDuration, delay: 4 })
+    gsap.to($mainTitle.children[3], { left: 1000 + 'px', duration: logoDuration, delay: 4.5 })
+    gsap.to($mainTitle.children[3], { left: 1500 + 'px', duration: logoDuration, delay: 5.5 })
+    gsap.to($mainTitle.children[3], { left: 2000 + 'px', duration: logoDuration, delay: 5.8 })
+
+    gsap.to($changeTitle.children[10], { top: 75 + 'px', duration: logoDuration, delay: 1 })
+    gsap.to($changeTitle.children[10], { left: 1400 + 'px', duration: logoDuration, delay: 6.5 })
+
+    gsap.to($changeTitle.children[2], { left: 120 + 'px', duration: logoDuration, delay: 2.5 })
+    gsap.to($changeTitle.children[2], { left: 320 + 'px', duration: logoDuration, delay: 3 })
+    gsap.to($changeTitle.children[2], { left: 450 + 'px', duration: logoDuration, delay: 4 })
+    gsap.to($changeTitle.children[2], { left: 550 + 'px', duration: logoDuration, delay: 4.5 })
+    gsap.to($changeTitle.children[2], { left: 740 + 'px', duration: logoDuration, delay: 5 })
+    gsap.to($changeTitle.children[2], { top: 75 + 'px', duration: logoDuration, delay: 5.5 })
+
+    gsap.to($changeTitle.children[3], { left: 184 + 'px', duration: logoDuration, delay: 2.5 })
+    gsap.to($changeTitle.children[3], { left: 384 + 'px', duration: logoDuration, delay: 3 })
+    gsap.to($changeTitle.children[3], { left: 514 + 'px', duration: logoDuration, delay: 4 })
+    gsap.to($changeTitle.children[3], { left: 614 + 'px', duration: logoDuration, delay: 4.5 })
+    gsap.to($changeTitle.children[3], { left: 830 + 'px', duration: logoDuration, delay: 5 })
+    gsap.to($changeTitle.children[3], { top: 75 + 'px', duration: logoDuration, delay: 5.5 })
+
+    gsap.to($changeTitle.children[8], { left: 243 + 'px', duration: logoDuration, delay: 2.5 })
+    gsap.to($changeTitle.children[8], { left: 443 + 'px', duration: logoDuration, delay: 3 })
+    gsap.to($changeTitle.children[8], { left: 573 + 'px', duration: logoDuration, delay: 4 })
+    gsap.to($changeTitle.children[8], { left: 673 + 'px', duration: logoDuration, delay: 4.5 })
+    gsap.to($changeTitle.children[8], { left: 917+ 'px', duration: logoDuration, delay: 5 })
+    gsap.to($changeTitle.children[8], { left: 1344.64 + 'px', duration: logoDuration, delay: 5.5 })
+    gsap.to($changeTitle.children[8], { top: 75 + 'px', duration: logoDuration, delay: 6 })
+    gsap.to($changeTitle.children[8], { left: 1250+ 'px', duration: logoDuration, delay: 6.5 })
+
+  }
+})
