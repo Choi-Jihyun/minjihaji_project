870ab3baf02b5fa7297fd0075e4aa7ca9f6328e5
nox590276 <nox590276@naver.com>
2023-06-30 02:50:50 +0900

    sub_broderline html,css 수정 js완료

---
+++ js/sub_lines.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded',()=>{
+  const $subDetailImgLeft=document.querySelectorAll('#sub_detail_line_cont_pic_left>img')
+  const $subDetailImgRight=document.querySelectorAll('#sub_detail_line_cont_pic_right>img')
+
+  let selectedImg=null;
+
+  for(item of $subDetailImgLeft){
+    item.addEventListener('mouseenter',activateImg)
+    item.addEventListener('mouseleave',notactivateImg)
+  }
+  for(item of $subDetailImgRight){
+    item.addEventListener('mouseenter',activateImg)
+    item.addEventListener('mouseleave',notactivateImg)
+  }
+
+  function activateImg(){
+    if(selectedImg != null || selectedImg != this){
+      gsap.to(selectedImg,{scale:(1)})
+    }
+
+    if(selectedImg !=this){
+      selectedImg=this
+      gsap.to(this,{scale:(1,1.1),ease:'power1.out'})
+    }
+
+  }
+
+  function notactivateImg(){
+    gsap.to(this,{scale:(1),ease:'power1.out'})
+  }
+
+
+})
