60fe364ac67744a2fd017a151d9e41eba730e4c8
nox590276 <nox590276@naver.com>
2023-06-30 11:37:02 +0900

    main_logo position수정

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   const svgPath = document.querySelector("#logo_svg_wrap > svg > path");
   const svgLogo = document.querySelector('.svg_logo')
-
-  const $logoBox = document.querySelector('#logo_box')
+//[0629_2]필요없는 변수 수정
   const $mainTitle = document.querySelector('#title')
   const $changeTitle = document.querySelector('#changeTitle')
-  const $mainLogo = document.querySelector('#main_logo')
 
-  const $scrollBox = document.querySelector('#scroll_box')
   const $scrollBar = document.querySelector('#scroll_bar')
   const $scrollText = document.querySelector('#scroll_text')
 
@@ -36,7 +33,7 @@ document.addEventListener('DOMContentLoaded', () => {
     gsap.to($mainTitle.children[11], { left: 690 + 'px', duration: logoDuration, delay: 2 })
     gsap.to($mainTitle.children[11], { left: 350 + 'px', duration: logoDuration, delay: 2.5 })
     gsap.to($mainTitle.children[11], { left: 315.43 + 'px', duration: logoDuration, delay: 3 })
-    gsap.to($mainTitle.children[11], { top: 0 + 'px', left: 122.5 + 'px', duration: logoDuration, delay: 4 })
+    gsap.to($mainTitle.children[11], { top: 0 + 'px', left: 100.5 + 'px', duration: logoDuration, delay: 4 })
 
     gsap.to($mainTitle.children[10], { top: 76 + 'px', duration: logoDuration, delay: 1 })
     gsap.to($mainTitle.children[10], { left: 718.15 + 'px', duration: logoDuration, delay: 1.5 })
@@ -45,7 +42,7 @@ document.addEventListener('DOMContentLoaded', () => {
     gsap.to($mainTitle.children[10], { left: 280 + 'px', duration: logoDuration, delay: 3 })
     gsap.to($mainTitle.children[10], { left: 280 + 'px', duration: logoDuration, delay: 3 })
     gsap.to($mainTitle.children[10], { top: 0 + 'px', left: 0 + 'px', duration: logoDuration, delay: 4 })
-    gsap.to($mainTitle.children[10], { left: 20 + 'px', duration: logoDuration, delay: 6.5 })
+    gsap.to($mainTitle.children[10], { left: 10 + 'px', duration: logoDuration, delay: 6.5 })
 
 
     gsap.to($mainTitle.children[0], { top: -76 + 'px', duration: logoDuration, delay: 1.5 })
@@ -61,7 +58,7 @@ document.addEventListener('DOMContentLoaded', () => {
     gsap.to($mainTitle.children[1], { left: 315.43 + 'px', duration: logoDuration, delay: 2 })
     gsap.to($mainTitle.children[1], { left: 477.46 + 'px', duration: logoDuration, delay: 2.5 })
     gsap.to($mainTitle.children[1], { left: 560.95 + 'px', duration: logoDuration, delay: 3 })
-    gsap.to($mainTitle.children[1], { top: 0 + 'px', left: 642.95 + 'px', duration: logoDuration, delay: 4 })
+    gsap.to($mainTitle.children[1], { top: 0 + 'px', left: 652.95 + 'px', duration: logoDuration, delay: 4 })
 
     gsap.to($mainTitle.children[6], { top: -76 + 'px', duration: logoDuration, delay: 1.5 })
 
@@ -79,7 +76,7 @@ document.addEventListener('DOMContentLoaded', () => {
     gsap.to($mainTitle.children[8], { top: -76 + 'px', duration: logoDuration, delay: 2.2 })
     gsap.to($mainTitle.children[8], { left: 893.26 + 'px', duration: logoDuration, delay: 2.5 })
     gsap.to($mainTitle.children[8], { top: 0 + 'px', duration: logoDuration, delay: 3 })
-    gsap.to($mainTitle.children[8], { left: 801.64 + 'px', duration: logoDuration, delay: 6.5 })
+    gsap.to($mainTitle.children[8], { left: 840.64 + 'px', duration: logoDuration, delay: 6.5 })
 
     gsap.to($mainTitle.children[9], { top: 76 + 'px', duration: logoDuration, delay: 2.5 })
     gsap.to($mainTitle.children[9], { left: 642.95 + 'px', duration: logoDuration, delay: 3.3 })
@@ -101,20 +98,20 @@ document.addEventListener('DOMContentLoaded', () => {
     gsap.to($mainTitle.children[3], { left: 2000 + 'px', duration: logoDuration, delay: 5.8 })
 
     gsap.to($changeTitle.children[10], { top: 75 + 'px', duration: logoDuration, delay: 1 })
-    gsap.to($changeTitle.children[10], { left: 1400 + 'px', duration: logoDuration, delay: 6.5 })
+    gsap.to($changeTitle.children[10], { left: 1460 + 'px', duration: logoDuration, delay: 6.5 })
 
     gsap.to($changeTitle.children[2], { left: 120 + 'px', duration: logoDuration, delay: 2.5 })
     gsap.to($changeTitle.children[2], { left: 320 + 'px', duration: logoDuration, delay: 3 })
     gsap.to($changeTitle.children[2], { left: 450 + 'px', duration: logoDuration, delay: 4 })
     gsap.to($changeTitle.children[2], { left: 550 + 'px', duration: logoDuration, delay: 4.5 })
-    gsap.to($changeTitle.children[2], { left: 740 + 'px', duration: logoDuration, delay: 5 })
+    gsap.to($changeTitle.children[2], { left: 715 + 'px', duration: logoDuration, delay: 5 })
     gsap.to($changeTitle.children[2], { top: 75 + 'px', duration: logoDuration, delay: 5.5 })
 
     gsap.to($changeTitle.children[3], { left: 184 + 'px', duration: logoDuration, delay: 2.5 })
     gsap.to($changeTitle.children[3], { left: 384 + 'px', duration: logoDuration, delay: 3 })
     gsap.to($changeTitle.children[3], { left: 514 + 'px', duration: logoDuration, delay: 4 })
     gsap.to($changeTitle.children[3], { left: 614 + 'px', duration: logoDuration, delay: 4.5 })
-    gsap.to($changeTitle.children[3], { left: 830 + 'px', duration: logoDuration, delay: 5 })
+    gsap.to($changeTitle.children[3], { left: 815 + 'px', duration: logoDuration, delay: 5 })
     gsap.to($changeTitle.children[3], { top: 75 + 'px', duration: logoDuration, delay: 5.5 })
 
     gsap.to($changeTitle.children[8], { left: 243 + 'px', duration: logoDuration, delay: 2.5 })
@@ -124,9 +121,10 @@ document.addEventListener('DOMContentLoaded', () => {
     gsap.to($changeTitle.children[8], { left: 917 + 'px', duration: logoDuration, delay: 5 })
     gsap.to($changeTitle.children[8], { left: 1344.64 + 'px', duration: logoDuration, delay: 5.5 })
     gsap.to($changeTitle.children[8], { top: 75 + 'px', duration: logoDuration, delay: 6 })
-    gsap.to($changeTitle.children[8], { left: 1250 + 'px', duration: logoDuration, delay: 6.5 })
+    gsap.to($changeTitle.children[8], { left: 1280 + 'px', duration: logoDuration, delay: 6.5 })
   }
 
+  //[지혜]scroll_box animation
   function activateScrollDown() {
     gsap.set($scrollBar, {display:'block', height: 0 + 'px' })
     gsap.to($scrollBar, {
@@ -141,3 +139,4 @@ document.addEventListener('DOMContentLoaded', () => {
     gsap.to($scrollText, { duration: 1, bottom: 0 + 'px', ease: 'bounce' })
   }
 })
+
