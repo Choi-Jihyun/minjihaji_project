da6c8af43c968a59bb875b2fb84587d36922e4be
osung_jihyun <61964114+Choi-Jihyun@users.noreply.github.com>
2023-06-30 18:16:05 +0900

    jihyun2 금욜저녁

--- js/core_values.js
+++ js/core_values.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
   const titleValue = document.querySelector("#title_values");
   const coreValuesText = document.querySelector("#core_values_text");
   
-  gsap.set(coreValuesStar, {display: 'none'})
+  gsap.set(coreValuesStar, {display: 'none', scale: 0})
   gsap.set(titleCore, {display: 'none', opacity: 0, top: 100})
   gsap.set(titleValue, {display: 'none', opacity: 0, bottom: -11})
   gsap.set(coreValuesText, {display: 'none', opacity: 0, bottom: -100})
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
         
       gsap.to(coreValuesLine, {width: 848, duration: 1, onComplete: ()=>{
         gsap.set(coreValuesStar, {display: "block"})
-        gsap.to(coreValuesStar, {rotation: 360, delay: 0.3, onComplete: ()=>{
+        gsap.to(coreValuesStar, {rotation: 360, scale: 1, delay: 0.3, onComplete: ()=>{
           for(const item of coreValuesCards){
             gsap.set(item, {display: 'block'})
               gsap.to(item, {left: 0, duration: 2, ease: "none", onComplete: ()=>{
