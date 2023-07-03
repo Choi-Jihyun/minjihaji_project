2cebe09039c411f6e41e1fe2faf21b0d17e0d9a7
osung_jihyun <61964114+Choi-Jihyun@users.noreply.github.com>
2023-07-03 21:29:56 +0900

    밀리는 현상 수정, scale값 조정

--- js/ecofuturist_video.js
+++ js/ecofuturist_video.js
@@ -1,25 +1,29 @@
 document.addEventListener('DOMContentLoaded',()=>{
     const ecofuturistTextSvg=document.querySelector('#ecofuturist_text_svg')
-    
+    const evarea = document.querySelector("#ecofuturist_video_area");
     let maskVisible=true;
 
-    ecofuturistTextSvg.addEventListener('mouseover', videoMouseOver);
-    ecofuturistTextSvg.addEventListener('mouseleave', videoMouseLeave);
+    evarea.addEventListener('mouseover', videoMouseOver);
+    evarea.addEventListener('mouseleave', videoMouseLeave);
     ecofuturistTextSvg.addEventListener('click', showVideo);
 
     function videoMouseOver(){
-      gsap.to(ecofuturistTextSvg,{scale:1,});
+      gsap.to(ecofuturistTextSvg,{scale:1.03,});
     }
 
     function videoMouseLeave(){
-      gsap.to(ecofuturistTextSvg,{scale:0.9,});
+      gsap.to(ecofuturistTextSvg,{scale:1,});
     }
 
     function showVideo(){
       if(maskVisible){
-        gsap.to(ecofuturistTextSvg,{mackImage:'none',webkitMaskImage:'none',scale:1})
+        gsap.to(ecofuturistTextSvg,{mackImage:'none',webkitMaskImage:'none',scale:1, onComplete: ()=>{
+          gsap.set(evarea, {display: "none"})
+        }})
     }else{
-      gsap.to(ecofuturistTextSvg,{maskImage:'url("/minjihaji_project/images/ecofuturist.svg")',webkitMaskImage:'url("/minjihaji_project/images/ecofuturist.svg")',duration:1})
+      gsap.to(ecofuturistTextSvg,{maskImage:'url("/minjihaji_project/images/ecofuturist.svg")',webkitMaskImage:'url("/minjihaji_project/images/ecofuturist.svg")',duration:1, onComplete:()=>{
+        gsap.set(evarea, {display: "block"})
+      }})
     }
     maskVisible=!maskVisible;
     }
